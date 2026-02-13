0d9feb66b09c9022ad8ba44cbf612f5bb7260350
hezrn <hezreenjayabellera@gmail.com>
2026-02-13 08:00:43 +0000

    completed laravel routes, controller, and views

---
+++ myproject/resources/views/greet.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Greeting Page</title>
+</head>
+<body>
+    <h1>Hi, Welcome to Laravel Greeting Page!</h1>
+</body>
+</html>
---
+++ myproject/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GreetController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'Hello, Laravel!';
+});
+
+Route::get('/greet', [GreetController::class, 'greet']);
